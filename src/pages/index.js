c7a43917e8a1fb173188070eba0e04f5abe5e034
George Papadopoulos <georgepap2001@gmail.com>
2024-03-16 17:39:12 +0200

    Added main content on the index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import Head from 'next/head';
 import HeaderMenu from '@/components/public/HeaderMenu';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { Grid } from '@mui/material';
+import {
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText, Tooltip,
+  Typography
+} from '@mui/material';
 import Footer from '@/components/public/Footer';
+import { useTranslation } from 'next-i18next';
+import CheckIcon from '@mui/icons-material/Check';
+import { useRouter } from 'next/router';
 
 export default function IndexPage(){
+  const { t } = useTranslation('common')
+  const router = useRouter()
+  const lang = router.locale
+
   return (
     <>
       <Head>
@@ -20,6 +35,294 @@ export default function IndexPage(){
           md={12}
           xs={12}
         >
+          <br/>
+          <br/>
+        </Grid>
+        <Grid
+          item={true}
+          md={12}
+          xs={12}
+          textAlign={'center'}
+        >
+          <Typography
+            variant={'h4'}
+          >
+            {t('antivirus_antimalware_for_android_devices')}
+          </Typography>
+          <Typography
+            variant={'h6'}
+          >
+            {t('keep_your_phone_or_tablet_protected')}
+          </Typography>
+        </Grid>
+        <Grid
+          item={true}
+          md={4}
+          xs={4}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={4}
+          xs={4}
+          textAlign={'center'}
+          style={{backgroundImage: 'url(/static/app_logo_globe_removed_bg_40_opacity.png)', backgroundRepeat: "no-repeat", backgroundPosition: 'center', backgroundSize: "cover" }}
+        >
+          <Divider
+            variant={'inset'}
+            sx={{ color: '#000000' }}/>
+          <Typography
+            variant={'span'}
+            style={{marginTop: "2%", display: "block"}}
+          >
+            {t('your_device_contains_vulnerabilities')}
+          </Typography>
+          <br/>
+          <List dense={false}>
+            <ListItem>
+              <ListItemIcon>
+                <CheckIcon style={{color: "#3B8132"}} />
+              </ListItemIcon>
+              <ListItemText
+                primary={t('antivirus_keeps_your_app_safe')}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <CheckIcon style={{color: "#3B8132"}} />
+              </ListItemIcon>
+              <ListItemText
+                primary={t('lightweight_installation')}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <CheckIcon style={{color: "#3B8132"}} />
+              </ListItemIcon>
+              <ListItemText
+                primary={t('include_optimization_tools')}
+              />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid
+          item={true}
+          md={4}
+          xs={4}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={4}
+          xs={4}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={4}
+          xs={4}
+          textAlign={'center'}
+        >
+          <Grid
+            container={true}
+            spacing={0}
+          >
+            <Grid
+              item={true}
+              md={6}
+              xs={6}
+              textAlign={'right'}
+            >
+              {(lang === "en") &&
+                <>
+                  <Tooltip title={t('download_the_application')} placement={"top"}>
+                    <div>
+                      <a href={"https://play.google.com"} target={"_blank"}>
+                        <img src={'/static/en-google-play-badge.png'} width={'65%'}/>
+                      </a>
+                    </div>
+                  </Tooltip>
+                </>
+              }
+              {(lang === "el") &&
+                <>
+                  <Tooltip title={t('download_the_application')} placement={"top"}>
+                    <div>
+                      <a href={"https://play.google.com"} target={"_blank"}>
+                        <img src={'/static/el-google-play-badge.png'} width={'65%'}/>
+                      </a>
+                    </div>
+                  </Tooltip>
+                </>
+              }
+            </Grid>
+            <Grid
+              item={true}
+              md={6}
+              xs={6}
+              textAlign={'left'}
+            >
+              {(lang === "en") &&
+                <>
+                  <Tooltip title={t('download_the_application')} placement={"top"}>
+                    <div>
+                      <a href={"https://play.google.com"} target={"_blank"}>
+                        <img src={'/static/en-download-it-now.png'} width={'73%'}
+                             style={{ marginTop: '-2%' }}/>
+                      </a>
+                    </div>
+                  </Tooltip>
+                </>
+              }
+              {(lang === "el") &&
+                <>
+                  <Tooltip title={t('download_the_application')} placement={"top"}>
+                    <div>
+                      <a href={"https://play.google.com"} target={"_blank"}>
+                        <img src={'/static/el-download-it-now.png'} width={'73%'} style={{marginTop: "-2%"}}/>
+                      </a>
+                    </div>
+                  </Tooltip>
+                </>
+              }
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item={true}
+          md={4}
+          xs={4}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={12}
+          xs={12}
+        >
+          <br/>
+          <br/>
+          <br/>
+        </Grid>
+        <Grid
+          item={true}
+          md={2}
+          xs={2}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={10}
+          xs={10}
+        >
+          <Grid
+            container={true}
+            spacing={0}
+          >
+            <Grid
+              item={true}
+              md={6}
+              xs={6}
+              textAlign={'right'}
+            >
+              <img src={'/static/security-shield-banner.png'} style={{marginRight: "10%"}} />
+            </Grid>
+            <Grid
+              item={true}
+              md={6}
+              xs={6}
+              textAlign={'left'}
+            >
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <Typography
+                variant={'h6'}
+                textAlign={'left'}
+                style={{marginRight: "45%", display: "block", marginLeft: "-15%"}}
+              >
+                {t('security_shield_description')}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item={true}
+          md={2}
+          xs={2}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={12}
+          xs={12}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={2}
+          xs={2}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={10}
+          xs={10}
+        >
+          <Grid
+            container={true}
+            spacing={0}
+          >
+            <Grid
+              item={true}
+              md={6}
+              xs={6}
+              textAlign={'right'}
+            >
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <Typography
+                variant={'h6'}
+                textAlign={'right'}
+                style={{marginRight: "20%", marginLeft: "15%"}}
+              >
+                {t('financial_security_description')}
+              </Typography>
+            </Grid>
+            <Grid
+              item={true}
+              md={6}
+              xs={6}
+              textAlign={'left'}
+            >
+              <img src={'/static/financial_security.png'} style={{display: "block", marginLeft: "-7%"}} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item={true}
+          md={2}
+          xs={2}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={12}
+          xs={12}
+        >
+        </Grid>
+        <Grid
+          item={true}
+          md={12}
+          xs={12}
+        >
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
           <br/>
           <br/>
           <br/>
